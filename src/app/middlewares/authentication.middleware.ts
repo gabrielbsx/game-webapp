8be2fa6e867a3abfe67b91858dc8404fb4436d55
Gabriel Barbosa <gabrielturing@gmail.com>
2025-09-14 23:15:31 -0300

    feat: streamline authentication middleware by removing redundant checks and enhancing error handling

--- src/app/middlewares/authentication.middleware.ts
+++ src/app/middlewares/authentication.middleware.ts
@@ -1,46 +1,43 @@
 import jwt from "jsonwebtoken";
 import {
   goNext,
-  unauthorized,
   type HttpMiddlewareResponseContract,
   type HttpRequestContract,
 } from "../contracts/http.contract.ts";
 import { userRepository } from "@/infra/database/repository/user.repository.ts";
+import { UnauthorizedException } from "@/core/errors/generic/unauthorized.exception.ts";
 
 export const authenticationMiddleware = async ({
   headers,
   setAuthenticatedUser,
 }: HttpRequestContract): Promise<HttpMiddlewareResponseContract> => {
-  try {
-    const authHeader = String(headers.authorization);
-
-    if (!authHeader) {
-      return unauthorized("Unauthorized");
-    }
-
-    const [, token] = authHeader.split(" ");
-
-    if (!token) {
-      return unauthorized("Unauthorized");
-    }
-
-    const decoded = jwt.decode(token);
-
-    if (!decoded || typeof decoded === "string" || !decoded.sub) {
-      return unauthorized("Unauthorized");
-    }
-
-    if (!(await userRepository.getUserById(decoded.sub))) {
-      return unauthorized("Unauthorized");
-    }
-
-    setAuthenticatedUser({
-      id: decoded.sub,
-    });
-
-    return goNext();
-  } catch (error) {
-    console.error(error);
-    return unauthorized("Unauthorized");
+  if (!headers?.authorization) {
+    throw new UnauthorizedException();
   }
+
+  const authHeader = String(headers.authorization);
+
+  if (!authHeader) {
+    throw new UnauthorizedException();
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  if (!token) {
+    throw new UnauthorizedException();
+  }
+
+  const decoded = jwt.decode(token);
+
+  if (!decoded || typeof decoded === "string" || !decoded.sub) {
+    throw new UnauthorizedException();
+  }
+
+  await userRepository.findByIdOrThrow(decoded.sub);
+
+  setAuthenticatedUser({
+    id: decoded.sub,
+  });
+
+  return goNext();
 };
--- src/infra/http/fastify/middleware.ts
+++ src/infra/http/fastify/middleware.ts
@@ -3,6 +3,7 @@ import {
   type HttpMiddlewareResponseContract,
   type HttpRequestContract,
 } from "@/app/contracts/http.contract.ts";
+import { HttpException } from "@/core/errors/generic/http.exception.ts";
 import type { FastifyRequest, FastifyReply } from "fastify";
 
 export const fastifyMiddlewareWrapper =
@@ -29,6 +30,11 @@ export const fastifyMiddlewareWrapper =
       return reply.status(response.statusCode).send(response.data);
     } catch (error) {
       request.log.error(error);
+
+      if (error instanceof HttpException) {
+        return reply.status(error.statusCode).send({ error: error.message });
+      }
+
       return reply.status(500).send({ error: "Internal Server Error" });
     }
   };
